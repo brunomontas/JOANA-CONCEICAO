f94b4d1df8f76903e168f05364a8884012f96f51
Bruno Monteiro <brunomon@gmail.com>
2020-09-13 18:06:30 +0100

    labirinto a funciona desktop

--- src/pages/labirinto.js
+++ src/pages/labirinto.js
@@ -4,14 +4,21 @@ import LabirintoDesktop from '../components/labirintoDesktop'
 import LabirintoMobile from "../components/labirintomobile"
 import Navigator from "../components/navigation"
 import Temple from "../components/temple"
+import labirintoDesktop from "../images/labirinto-desktop.svg"
 
 
 export default ()=> {
   return <div>
         <Navigator></Navigator>
         <Temple>
-        <LabirintoDesktop className="desktop-display"/>
-          <LabirintoMobile className="mobile-display"/>
+          <LabirintoDesktop className="desktop-display"/>
         </Temple>
+
+        {/*
+        <Layout>
+          <LabirintoDesktop className="desktop-display"/>
+          <LabirintoMobile className="mobile-display"/>
+        </Layout>
+        */}
       </div>
 }
