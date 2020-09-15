edf33b8149598e71fda65d9bc849467bf5fb0d1f
Bruno Monteiro <brunomon@gmail.com>
2020-09-15 10:06:17 +0100

    videos

--- src/pages/guia-interior.js
+++ src/pages/guia-interior.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from '../components/layout.js'
 import Navigator from "../components/navigationLaberith"
 import Temple from "../components/temple"
-import VideoLink from "../images/guiainterior2.mp4"
+import VideoLink from "../images/interior.mp4"
 
 
 
